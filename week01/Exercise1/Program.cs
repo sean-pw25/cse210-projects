97d8b046e1cc8944f9eaef77017631845bdbdda2
sean-pw25 <swood3@byupathway.edu>
2026-05-04 19:17:01 -0700

    Finished C# Programming Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string f_name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string l_name = Console.ReadLine();
+
+        Console.Write($"Your name is {l_name}, {f_name} {l_name}.");
     }
 }
