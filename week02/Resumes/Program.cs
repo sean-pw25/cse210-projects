ccd82b01bd1c424df7b456cc51f3fa9654c5ad70
sean-pw25 <swood3@byupathway.edu>
2026-05-13 16:51:53 -0700

    completed learning activity 02

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,26 @@
-using System;
+using System.ComponentModel;
 
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Warehouse Worker";
+        job1._company = "Peekaboo";
+        job1._startYear = 2020;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Shelf Stocker";
+        job2._company = "Wal-Mart";
+        job2._startYear = 2026;
+        job2._endYear = 2026;
+
+        Resume myResume = new Resume();
+        myResume._name = "Sean Wood";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
