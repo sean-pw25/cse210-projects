1e7e561056a43fcf0c812bff4686ab64de2eb931
sean-pw25 <swood3@byupathway.edu>
2026-05-13 17:04:02 -0700

    began journal project

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Entry
+{
+    string _date;
+    string _promptText;
+    string _entryText;
+
+    public void Display()
+    {
+
+    }
+}
---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Journal
+{
+    List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+    public void DisplayAll()
+    {
+
+    }
+    public void SaveToFile(string file)
+    {
+
+    }
+    public void LoadFromFile(string file)
+    {
+
+    }
+}
