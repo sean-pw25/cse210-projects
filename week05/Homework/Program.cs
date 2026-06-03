38e30e2faf668acf37d8e45a7ec2b1b1098a7c16
sean-pw25 <swood3@byupathway.edu>
2026-06-02 19:16:29 -0700

    finished learning activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -2,7 +2,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Assignment assignment = new Assignment("Samuel Bennet", "Multiplication");
-        Console.WriteLine(assignment.GetSummary());
+        MathAssignment mathAssignment = new MathAssignment("Samuel Bennet", "Multiplication", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
