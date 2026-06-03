487e5c79710881972ecc3fc202cc43a3c3666888
sean-pw25 <swood3@byupathway.edu>
2026-06-02 18:21:50 -0700

    began project

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,8 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
     }
 }
