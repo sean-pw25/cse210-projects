70c8e37065c4b2275d69e3333dc6628f3a5b0eef
sean-pw25 <swood3@byupathway.edu>
2026-06-11 19:38:28 -0700

    finished polymorphism learning activity

---
+++ week06/Shapes/Circle.cs
@@ -0,0 +1,16 @@
+public class Circle : Shape
+{
+    private double _radius;
+
+
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+
+
+    public override double GetArea()
+    {
+        return Math.Round(_radius * _radius * Math.PI, 2);
+    }
+}
--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("red", 4);
+        Rectangle rectangle = new Rectangle("blue", 4, 2);
+        Circle circle = new Circle("green", 9);
+
+        List<Shape> shapes = new List<Shape>(3);
+        shapes = [square, rectangle, circle];
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
     }
 }
